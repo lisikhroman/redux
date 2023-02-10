a2cd54d9ccdd6d906ea6ead7e53bac89fa3294e7
DESKTOP-CUGMPBP\79234 <Roman.Lisikh@indusoft.ru>
2023-02-10 14:21:58 +0700

    redux lesson 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const reducer = (state = 0, action) => {
+    switch (action.type) {
+        case 'INC':
+            return state + 1;
+        default:
+            return state;
+    }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const store = createStore(reducer);
+console.log(store.getState());
+
+store.dispatch({type: 'INC'});
+console.log(store.getState())
+store.dispatch({type: 'INC'});
+console.log(store.getState())
+store.dispatch({type: 'INC'});
+console.log(store.getState())
+store.dispatch({type: 'INC'});
+console.log(store.getState())
